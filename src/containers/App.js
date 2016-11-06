82f2ccef2a1391614b0e3d5503adad4e3030a279
David Bonnemaison <david.bonnemaison@gmail.com>
2016-11-06 11:57:04 +0100

    Initial commit for learning numbers app

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+export default class App extends Component {
+
+  render() {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
